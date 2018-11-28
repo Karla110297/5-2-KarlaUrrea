279e25abfc30b474e1d7a6bc533a9f8861dcb33b
Karla110297 <dinorakarla@hotmail.com>
2018-11-28 10:39:47 -0800

    QuickSort
    Quick sort
    ordenamiento

--- 5-2-KarlaUrrea/Program.cs
+++ 5-2-KarlaUrrea/Program.cs
@@ -10,6 +10,29 @@ namespace _5_2_KarlaUrrea
     {
         static void Main(string[] args)
         {
+            //arreglos a ordenar
+            double[] A = { 11, 2, 3, 14, 18, 45, -3, -1, 6, 9 };
+            double[] B = { 1, 3, 5, 7, 8, 3, 9, .7071, 16.5664, 12, 0, 10 };
+            double[] C = { 3, 7, 15, 4.0/3.0, 155, 100, 15, Math.Sqrt(3), 5, 2, 3, 2 };//se uso la clase math para poder hacer la raiz y se tuvo que agregar decimales a la division
+            double[] D = { 8, 19, 7, 3, 15, 23, 34, 27, 89, 101 };
+
+            QuickSort qs = new QuickSort();//crea la clase            
+
+            qs.Imprimir(A);//manda imprimir elementos de arreglo A
+            Console.WriteLine();
+
+            qs.Imprimir(B);//manda imprimir elementos de arreglo B
+            Console.WriteLine()
+
+            qs.Imprimir(C);//manda imprimir elementos de arreglo C
+            Console.WriteLine();
+
+            qs.Imprimir(D);//manda imprimir elementos de arreglo D
+
+
+
+            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
---
+++ 5-2-KarlaUrrea/QuickSort.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2_KarlaUrrea
+{
+    class QuickSort
+    {
+        public  QuickSort(){}
+     
+        
+
+        public void Imprimir(double[] X)// imprime el arreglo ordenado
+        {
+            for (int i = 0; i < X.Length; i++)//imprime arreglo sin ordenar
+            {
+                Console.Write(X[i] + ", ");
+            }
+            quicksort(X, 0, X.Length - 1);//manda llamar funcion quicksort para ordenar
+            Console.WriteLine("\nOrdenado");//imprime arreglo ordenado
+            for (int j=0; j < X.Length; j++)
+            {
+                Console.Write("{0} ", X[j]);
+            }
+            Console.ReadLine();
+        }
+        public void quicksort(double[]arreglo, int primero, int ultimo)//ordena el arreglo y para eso pide el arreglo, el primer indice y el ultimo
+        {
+            int i = primero, j = ultimo;
+
+            int indice = (primero+ultimo)/2;//se realiza un indice para utilizar como pivote
+
+            double pivote = arreglo[indice];
+            double auxiliar;//variable temporal
+            do
+            {
+                while (arreglo[i] < pivote) { i++; }//recorre arreglo
+                while (arreglo[j] > pivote) { j--; }//recorre arreglo
+
+                if (i <= j)//cambia posiciones
+                {
+                    auxiliar = arreglo[j];
+                    arreglo[j] = arreglo[i];
+                    arreglo[i] = auxiliar;
+                    i++;
+                    j--;
+                }
+
+            } while (i <= j);
+
+            if (primero < j) { quicksort(arreglo, primero, j); }
+            if (ultimo > i) { quicksort(arreglo, i, ultimo); }
+
+        }
+
+        
+
+
+    }
+}
