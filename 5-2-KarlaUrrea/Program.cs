3b1a89665125d9a3ad4ae2fd3737fdef5edc91c4
Karla110297 <dinorakarla@hotmail.com>
2018-11-27 22:54:08 -0800

    Add project files.

---
+++ 5-2-KarlaUrrea/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2_KarlaUrrea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
